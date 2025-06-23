035bccc0a6c2c21a9d453db957c18f54f61c22e0
Sami Afzal <ranasami0909@gmail.com>
2025-06-23 22:17:23 +0000

    add affect on side bar when we click on three dots on the top right of t

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -7,52 +7,27 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
-import { CircleUser, Menu, Wallet } from 'lucide-react';
+import { CircleUser, Menu } from 'lucide-react';
 import Link from 'next/link';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Sidebar } from '../layout/sidebar';
 
 export function Header() {
   return (
-    <header className="flex h-14 items-center gap-4 border-b bg-card px-4 lg:h-[60px] lg:px-6">
+    <header className="flex h-14 items-center gap-4 border-b bg-background px-4 lg:h-[60px] lg:px-6">
         <Sheet>
           <SheetTrigger asChild>
             <Button
               variant="outline"
               size="icon"
-              className="shrink-0 md:hidden"
+              className="shrink-0"
             >
               <Menu className="h-5 w-5" />
               <span className="sr-only">Toggle navigation menu</span>
             </Button>
           </SheetTrigger>
-          <SheetContent side="left" className="flex flex-col">
-            <nav className="grid gap-2 text-lg font-medium">
-              <Link
-                href="#"
-                className="flex items-center gap-2 text-lg font-semibold mb-4"
-              >
-                <Wallet className="h-6 w-6 text-primary" />
-                <span className="sr-only">SpendWise</span>
-              </Link>
-              <Link
-                href="/dashboard"
-                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-              >
-                Dashboard
-              </Link>
-              <Link
-                href="/dashboard/records"
-                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-              >
-                Monthly Records
-              </Link>
-              <Link
-                href="/dashboard/profile"
-                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-              >
-                Profile
-              </Link>
-            </nav>
+          <SheetContent side="left" className="flex flex-col p-0 w-[280px]">
+            <Sidebar />
           </SheetContent>
         </Sheet>
         
